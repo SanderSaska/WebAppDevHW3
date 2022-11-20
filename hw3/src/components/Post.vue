f16534f31c59a5061b49063648d6b50c43da55f8
SanderSaska <sander.saska@gmail.com>
2022-11-20 21:08:13 +0200

    Added fetch and reset likes
    Added fetch from Vuex Global Store and Reset Likes button

---
+++ hw3/src/components/Post.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="Content">
+        <aside class="Aside">
+        </aside>
+        <main class="Main" id="main">
+            <article class="Article" v-for="post in postsList" :key="post.id">
+                <section class="UserDate">
+                    <img class="user_icon" v-bind:src="post.username_source" width="25" height="25" alt="User icon">
+                    <p>{{post.username}}&ensp;&ensp;&ensp;</p>
+                    <p>{{post.date}}</p>
+                </section>
+                <img v-bind:src="post.img_source" width="200" height="200">
+                <p>{{post.description}}</p>
+                <section class="Likes">
+                    <a class="like"><img src="../assets/Like-Button-Transparent.png" width="25" height="25" alt="Like"></a>
+                    <p>{{post.likes}}</p>
+                </section>
+            </article>
+        </main>
+        <aside class="Aside">
+
+        </aside>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Post",
+    data: function() {
+        return {
+        }
+    },
+computed: {
+    postsList(){
+        return this.$store.state.postsList
+    }
+}
+}
+</script>
+
+<style scoped>
+* {
+    font-family: 'Courier New', Courier, monospace;
+}
+
+p {
+    line-height: 1.25;
+}
+
+::placeholder {
+    color: grey;
+}
+
+</style>
--- hw3/src/views/HomeView.vue
+++ hw3/src/views/HomeView.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <Header/>
     <h1>This is main page</h1>
+    <Post/>
+    <button v-on:click="ResetLikes">Reset likes</button>
     <Footer/>
   </div>
 </template>
@@ -10,12 +12,24 @@
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
-
+import Post from '@/components/Post.vue'
 export default {
   name: 'HomeView',
   components: {
     Header,
-    Footer
+    Footer,
+    Post
+  },
+  data: function () {
+    return {
+
+    }
+  },
+
+  methods: {
+    ResetLikes: function () {
+      this.$store.dispatch("ResetLikesAct")
+    },
   }
 }
 </script>
